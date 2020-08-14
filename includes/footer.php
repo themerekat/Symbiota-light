c7b388313bc5b44e9a2014f06b6cc74e7035ff29
Edward Gilbert <egbot@asu.edu>
2020-08-14 10:30:29 -0700

    Minor adjustments to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-    	</td>
+		</td>
 	</tr>
 	<tr>
 		<td id="footer" colspan="3">
@@ -7,22 +7,22 @@
 				<a href="http://idigbio.org" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" style="width:150px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:180px" /></a>
 			</p>
-                        <div style="width:650px;height:50px;margin-top:10px;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;" >
-                                        The PCC, and this data portal, were made possible by funding from the National Science Foundation’s
-                                        Advancing Digitization of Biological Collections (ADBC) program, grant numbers
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802504&HistoricalAwards=false" target="_blank">1802504</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802352&HistoricalAwards=false" target="_blank">1802352</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802134&HistoricalAwards=false" target="_blank">1802134</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802033&HistoricalAwards=false" target="_blank">1802033</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802270&HistoricalAwards=false" target="_blank">1802270</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802255&HistoricalAwards=false" target="_blank">1802255</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802239&HistoricalAwards=false" target="_blank">1802239</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802446&HistoricalAwards=false" target="_blank">1802446</a>,
-                                        <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802305&HistoricalAwards=false" target="_blank">1802305</a>.
-                          </div>
-                          <div style="width:650px;height:50px;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;" >
-                                  The pteridoportal taxonomic thesaurus is based on the <a href=https://worldplants.webarchiv.kit.edu/ferns/>Checklist of Ferns and Lycophytes of the World</a>, generously provided by Michael Hassler.
-                          </div>
+			<div style="width:650px;height:50px;margin-top:10px;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;" >
+				The PCC, and this data portal, were made possible by funding from the National Science Foundation’s
+				Advancing Digitization of Biological Collections (ADBC) program, grant numbers
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802504&HistoricalAwards=false" target="_blank">1802504</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802352&HistoricalAwards=false" target="_blank">1802352</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802134&HistoricalAwards=false" target="_blank">1802134</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802033&HistoricalAwards=false" target="_blank">1802033</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802270&HistoricalAwards=false" target="_blank">1802270</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802255&HistoricalAwards=false" target="_blank">1802255</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802239&HistoricalAwards=false" target="_blank">1802239</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802446&HistoricalAwards=false" target="_blank">1802446</a>,
+				<a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1802305&HistoricalAwards=false" target="_blank">1802305</a>.
+			</div>
+			<div style="width:650px;height:50px;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;" >
+				The pteridoportal taxonomic thesaurus is based on the <a href="https://worldplants.webarchiv.kit.edu/ferns/" target="_blank">Checklist of Ferns and Lycophytes of the World</a>, generously provided by Michael Hassler.
+			</div>
 		</td>
 	</tr>
 </table>
